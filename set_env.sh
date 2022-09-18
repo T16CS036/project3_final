b2d014512645d3b1a781ddfb7cddac0ea7b7c995
MinhDTH <dinhthaihoangminh@gmail.com>
2022-09-19 02:07:49 +0900

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME project3
+setx POSTGRES_PASSWORD 272309578712
+setx POSTGRES_HOST database-1.cd5amzkcep8i.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB database-1
+setx AWS_BUCKET preject3-272309578712-dev
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET testing
+setx URL http://localhost:8100
