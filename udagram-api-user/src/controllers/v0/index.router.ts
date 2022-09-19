e444a320902e8d81d430793e3807cd5e52ebea94
MinhDTH <dinhthaihoangminh@gmail.com>
2022-09-19 18:30:35 +0900

    update all after fix bug on local

--- udagram-api-user/src/controllers/v0/index.router.ts
+++ udagram-api-user/src/controllers/v0/index.router.ts
@@ -1,4 +1,4 @@
-import {Router, Request, Response} from 'express';
+import {Request, Response, Router} from 'express';
 import {UserRouter} from './users/routes/user.router';
 
 const router: Router = Router();
@@ -6,7 +6,7 @@ const router: Router = Router();
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
-  res.send(`V0`);
+    res.send(`V0`);
 });
 
 export const IndexRouter: Router = router;
